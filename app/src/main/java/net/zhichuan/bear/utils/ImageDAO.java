7a24b3b88f6f77548b4899c407710ae0632948d4
Zhichuan Zhao <zhaozhichuan@gmail.com>
2023-08-01 09:28:21 -0400

    undo javadoc

--- app/src/main/java/net/zhichuan/bear/utils/ImageDAO.java
+++ app/src/main/java/net/zhichuan/bear/utils/ImageDAO.java
@@ -8,15 +8,31 @@ import androidx.room.Query;
 
 import java.util.List;
 
+/**
+ * This class is used to access the database.
+ * It uses the Room library to access the database.
+ */
 @Dao
 public interface ImageDAO {
+    /**
+     * Insert an image into the database.
+     * @param image The image to be inserted.
+     */
     @Insert
     void insert(@NonNull ImageEntity image);
 
+    /**
+     * Get all the images from the database.
+     * @return A list of all the images in the database.
+     */
     @NonNull
     @Query("SELECT * FROM ImageEntity")
     List<ImageEntity> getAllImages();
 
+    /**
+     * Delete an image from the database.
+     * @param image The image to be deleted.
+     */
     @Delete
     void delete(@NonNull ImageEntity image);
 }
